9179ca56864d5a4f943fc8a69aac9dd6824183fc
Nornort <francois.lalleve@laposte.net>
2020-03-12 13:39:32 +0100

    Remake migrations

---
+++ src/app/Task.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = ['nom', 'prenom', "email", "tel", "social", "type", "motif", "description"];
+}
---
+++ src/database/migrations/2020_03_12_103047_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->string("email");
+            $table->string("tel");
+            $table->string("social");
+            $table->string("type");
+            $table->string("motif");
+            $table->text("description");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
